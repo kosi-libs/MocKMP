d35f605fbc46f893b93d28cf1f98d98475aed1e9
Romain BOISSELLE <romain.boisselle@proton.me>
2022-10-05 08:34:31 +0200

    Kotlin 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("org.kodein.root")
-    id("com.google.devtools.ksp") version "1.7.20-RC-1.0.6" apply false
+    id("com.google.devtools.ksp") version "1.7.20-1.0.6" apply false
 }
 
-val kspVersion by extra { "1.7.20-RC-1.0.6" }
+val kspVersion by extra { "1.7.20-1.0.6" }
 
 allprojects {
     group = "org.kodein.mock"
-    version = "1.10.0-kotlin-1.7.20-RC"
+    version = "1.10.0"
 }
