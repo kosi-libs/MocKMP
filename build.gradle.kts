050b2709ff224f7d8b47eedee1474f55da168592
Salomon BRYS <sbrys-ext@deezer.com>
2022-11-30 12:12:15 +0100

    Kotlin 1.7.22 & KSP 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("org.kodein.root")
-    id("com.google.devtools.ksp") version "1.7.20-1.0.6" apply false
+    id("com.google.devtools.ksp") version "1.7.22-1.0.8" apply false
 }
 
-val kspVersion by extra { "1.7.20-1.0.6" }
+val kspVersion by extra { "1.7.22-1.0.8" }
 
 allprojects {
     group = "org.kodein.mock"
