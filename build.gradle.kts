c5c80ea3f8401230ba8e2d3409b24aa53923918f
Romain BOISSELLE <romain.boisselle@proton.me>
2023-01-01 20:22:48 +0100

    kotlin 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("org.kodein.root")
-    id("com.google.devtools.ksp") version "1.7.22-1.0.8" apply false
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8" apply false
 }
 
-val kspVersion by extra { "1.7.22-1.0.8" }
+val kspVersion by extra { "1.8.0-1.0.8" }
 
 allprojects {
     group = "org.kodein.mock"
-    version = "1.11.0"
+    version = "1.12.0"
 }
