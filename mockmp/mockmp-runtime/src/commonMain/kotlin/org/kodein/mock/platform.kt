dbd079e498136d136ddb4cd12def3c4bed6e9ac3
Salomon BRYS <salomon.brys@gmail.com>
2026-08-21 13:28:11 +0200

    refactor(runtime): introduce `RuntimeNoSTException` for cleaner exception handling across platforms

--- mockmp/mockmp-runtime/src/commonMain/kotlin/org/kodein/mock/platform.kt
+++ mockmp/mockmp-runtime/src/commonMain/kotlin/org/kodein/mock/platform.kt
@@ -5,3 +5,6 @@ import kotlin.reflect.KClass
 
 @PublishedApi
 internal expect fun KClass<*>.bestName(): String
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+internal expect open class RuntimeNoSTException(message: String?) : Exception
--- mockmp/mockmp-runtime/src/jvmMain/kotlin/org/kodein/mock/platformJvm.kt
+++ mockmp/mockmp-runtime/src/jvmMain/kotlin/org/kodein/mock/platformJvm.kt
@@ -5,3 +5,9 @@ import kotlin.reflect.KClass
 
 @PublishedApi
 internal actual fun KClass<*>.bestName(): String = qualifiedName ?: simpleName ?: "Unknown"
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+internal actual open class RuntimeNoSTException actual constructor(message: String?) : java.lang.RuntimeException(message) {
+    override fun fillInStackTrace(): Throwable? = this
+    override fun getStackTrace(): Array<out StackTraceElement?>? = null
+}
